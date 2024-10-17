0e3ef8e5caf92109bce75a1ecbda62a5d2adaaa3
johnwendel <john.alan.wendel@gmail.com>
2024-10-16 22:01:59 -0400

    add images

--- travel_recommendation.js
+++ travel_recommendation.js
@@ -1,17 +1,70 @@
 // JavaScript Document
-const searchInput = document.getElementById("search-input");
-const searchButton = document.getElementById("search-button");
+console.log("Starting javascript.....");
 
+document.addEventListener("DOMContentLoaded", function() {
+  const searchButton = document.getElementById("search-btn");
+  const searchInput = document.getElementById("search-input");
 
+  searchButton.addEventListener("click", () => {
+		fetch("./travel_recommendation.json")
+		   .then(response => response.json())
+		   .then(data => { 
+const searchTerm = document.getElementById("search-input").value.toLowerCase();
+
+			   console.log(data);
+               console.log(searchTerm);
+			
+			if (searchTerm === "countries" || searchTerm === "country" || searchTerm === "brazil" || searchTerm === "australia" || searchTerm === "japan") {
+			   const countryData = data.countries;
+			   let id1 = Math.floor(Math.random() * countryData.length);
+			   if (searchTerm === "brazil") id1 = 2;
+			   if (searchTerm === "japan") id1 = 1;
+			   if (searchTerm === "australia") id1 = 0;
+			   console.log("Found countries... id1 is "+id1);
+			   document.getElementById("image-1").src = countryData[id1].cities[0].imageUrl;
+			   document.getElementById("name-1").textContent = countryData[id1].cities[0].name;
+			   document.getElementById("desciption-1").textContent = countryData[id1].cities[0].description;
+						
+			   document.getElementById("image-2").src = countryData[id1].cities[1].imageUrl;
+			   document.getElementById("name-2").textContent = countryData[id1].cities[1].name;
+			   document.getElementById("desciption-2").textContent = countryData[id1].cities[1].description;
+		}
+	
+	
+	       else if (searchTerm === "temple" || searchTerm === "temples") {
+			   const templeData = data.temples;
+			   const id1 = 0;
+			   const id2 = 1;
+			   console.log("Found Temples... id1 is "+id1);
+			
+			   document.getElementById("image-1").src = templeData[id1].imageUrl;
+			   document.getElementById("name-1").textContent = templeData[id1].name;
+			   document.getElementById("desciption-1").textContent = templeData[id1].description;
+						
+			   document.getElementById("image-2").src = templeData[id2].imageUrl;
+			   document.getElementById("name-2").textContent = templeData[id2].name;
+			   document.getElementById("desciption-2").textContent = templeData[id2].description;
+		}
+		
+	
+	       else if (searchTerm === "beach" || searchTerm === "beaches") {
+			   console.log("33333333333");
+			   const beachData = data.beaches;
+			   const id1 = 0;
+			   const id2 = 1;
+			   console.log("Found beaches... id1 is "+id1);
+			
+			   document.getElementById("image-1").src = beachData[id1].imageUrl;
+			   document.getElementById("name-1").textContent = beachData[id1].name;
+			   document.getElementById("desciption-1").textContent = beachData[id1].description;
+						
+			   document.getElementById("image-2").src = beachData[id2].imageUrl;
+			   document.getElementById("name-2").textContent = beachData[id2].name;
+			   document.getElementById("desciption-2").textContent = beachData[id2].description;
+		}
+		
+    
+          else console.log("NONENONENONENONE");
  
-   fetch('./travel_recommendation_api.json')
-   .then(response => response.json())
-   .then(data => {
-     console.log(data);
-     // You can now work with the JSON data here
-   })
-   .catch(error => console.error('Error fetching JSON:', error));	   
-	   
-
-console.log("Here is the extracted data: ",data);
+    }    ) }  ) } );
 
